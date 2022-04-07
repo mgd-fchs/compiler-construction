6e1692278ac392b2bdb9b2e0a84706cc0d1d9284
Simon Gutschlhofer <simon.gutschlhofer@student.tugraz.at>
2022-04-07 19:31:35 +0200

    added bonus tests

--- src/test/java/at/tugraz/ist/cc/TypeCheckerBonusTest.java
+++ src/test/java/at/tugraz/ist/cc/TypeCheckerBonusTest.java
@@ -9,8 +9,10 @@ import static org.junit.Assert.assertTrue;
 
 public class TypeCheckerBonusTest {
 
-    private final String path_bonus = "src/test/resources/public/input/typechecking/bonus/";
-    
+    private final String path_fail_bonus = "src/test/resources/private/typechecking/fail/bonus/";
+    private final String path_pass_bonus = "src/test/resources/private/typechecking/pass/bonus/";
+    private final String path_warn_bonus = "src/test/resources/private/typechecking/warn/bonus/";
+
     TypeChecker typeChecker = new TypeChecker();
     boolean debug = true;
 
@@ -18,7 +20,7 @@ public class TypeCheckerBonusTest {
     public void testBonusPass01() {
         // check simple pass case (assignments)
         ErrorHandler.INSTANCE.reset();
-        int result = typeChecker.checkTypes(path_bonus + "bonus_pass01.jova", debug);
+        int result = typeChecker.checkTypes(path_pass_bonus + "bonus_pass01.jova", debug);
         assertEquals(0, result);
     }
 
@@ -26,7 +28,7 @@ public class TypeCheckerBonusTest {
     public void testBonusPass02() {
         // Define and retrieve class members of type float + return values
         ErrorHandler.INSTANCE.reset();
-        int result = typeChecker.checkTypes(path_bonus + "bonus_pass02.jova", debug);
+        int result = typeChecker.checkTypes(path_pass_bonus + "bonus_pass02.jova", debug);
         assertEquals(0, result);
     }
 
@@ -34,7 +36,7 @@ public class TypeCheckerBonusTest {
     public void testBonusPass03() {
         // Define and retrieve class members of type char + return values
         ErrorHandler.INSTANCE.reset();
-        int result = typeChecker.checkTypes(path_bonus + "bonus_pass03.jova", debug);
+        int result = typeChecker.checkTypes(path_pass_bonus + "bonus_pass03.jova", debug);
         assertEquals(0, result);
     }
 
@@ -42,7 +44,7 @@ public class TypeCheckerBonusTest {
     public void testBonusPass04() {
         // Check correct operators on float
         ErrorHandler.INSTANCE.reset();
-        int result = typeChecker.checkTypes(path_bonus + "bonus_pass04.jova", debug);
+        int result = typeChecker.checkTypes(path_pass_bonus + "bonus_pass04.jova", debug);
         assertEquals(0, result);
     }
 
@@ -50,7 +52,7 @@ public class TypeCheckerBonusTest {
     public void testBonusPass05() {
         // Check correct operators on char
         ErrorHandler.INSTANCE.reset();
-        int result = typeChecker.checkTypes(path_bonus + "bonus_pass05.jova", debug);
+        int result = typeChecker.checkTypes(path_pass_bonus + "bonus_pass05.jova", debug);
         assertEquals(0, result);
     }
 
@@ -59,7 +61,7 @@ public class TypeCheckerBonusTest {
         // check coercion for char
         // expect 2 warnings
         ErrorHandler.INSTANCE.reset();
-        int result = typeChecker.checkTypes(path_bonus + "bonus_warn01.jova", debug);
+        int result = typeChecker.checkTypes(path_warn_bonus + "bonus_warn01.jova", debug);
         assertEquals(0, result);
     }
 
@@ -68,7 +70,7 @@ public class TypeCheckerBonusTest {
         // check coercion for float
         // expect 5 warnings
         ErrorHandler.INSTANCE.reset();
-        int result = typeChecker.checkTypes(path_bonus + "bonus_warn02.jova", debug);
+        int result = typeChecker.checkTypes(path_warn_bonus + "bonus_warn02.jova", debug);
         assertEquals(0, result);
     }
 
@@ -76,7 +78,7 @@ public class TypeCheckerBonusTest {
     public void testBonusFail01() {
         // Define and retrieve class members of type char + return values
         ErrorHandler.INSTANCE.reset();
-        int result = typeChecker.checkTypes(path_bonus + "bonus_fail01.jova", debug);
+        int result = typeChecker.checkTypes(path_fail_bonus + "bonus_fail01.jova", debug);
         assertEquals(3, result);
     }
 
@@ -84,7 +86,7 @@ public class TypeCheckerBonusTest {
     public void testBonusFail02() {
         // Define and retrieve class members of type char + return values
         ErrorHandler.INSTANCE.reset();
-        int result = typeChecker.checkTypes(path_bonus + "bonus_fail02.jova", debug);
+        int result = typeChecker.checkTypes(path_fail_bonus + "bonus_fail02.jova", debug);
         assertEquals(6, result);
     }
 }
