d890eab8ddf32c3fa197f5ed891e938123a8cbf5
Magdalena Hinterkoerner <m.hinterkoerner@student.tugraz.at>
2022-04-21 16:19:06 +0200

    Setup test class

---
+++ src/test/java/at/tugraz/ist/cc/CodeGeneratorPrivateTest.java
@@ -0,0 +1,23 @@
+package at.tugraz.ist.cc;
+
+import at.tugraz.ist.cc.error.ErrorHandler;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class CodeGeneratorPrivateTest {
+
+    private final String path_fail = "src/test/resources/public/input/codegeneration/fail/";
+    private final String path_pass = "src/test/resources/public/input/codegeneration/pass/";
+    private final String path_out = "src/test/resources/public/out/codegeneration/";
+    CodeGenerator codeGenerator = new CodeGenerator();
+
+    @Test
+    public void testPass01() {
+        ErrorHandler.INSTANCE.reset();
+        int result = codeGenerator.createCode(path_pass + "pass01.jova", path_out + "pass01.j");
+        assertEquals(0, result);
+    }
+}
