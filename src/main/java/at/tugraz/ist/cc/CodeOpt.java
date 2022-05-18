7598dc0c8747c67bf9e9bb6551c48935653236d7
Magdalena Hinterkoerner <m.hinterkoerner@student.tugraz.at>
2022-05-18 15:31:34 +0200

    Implement constants folding

--- src/main/java/at/tugraz/ist/cc/CodeOpt.java
+++ src/main/java/at/tugraz/ist/cc/CodeOpt.java
@@ -1,12 +1,77 @@
 package at.tugraz.ist.cc;
 
+import at.tugraz.ist.cc.error.ErrorHandler;
+import at.tugraz.ist.cc.instructions.ArithmeticBinaryInstruction;
+import at.tugraz.ist.cc.instructions.BaseInstruction;
+import at.tugraz.ist.cc.instructions.BinaryInstruction;
+import at.tugraz.ist.cc.instructions.OperatorTypes;
+import at.tugraz.ist.cc.symbol_table.SymbolClass;
+import at.tugraz.ist.cc.symbol_table.SymbolTable;
+import at.tugraz.ist.cc.symbol_table.SymbolVariable;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author wehopeu
  *
  */
 public class CodeOpt {
+
     public int optimizeCode(String file_path, boolean debug, String out_path) {
-        // TODO: implement Task 3 here...
+        SymbolTable.reset();
+
+        LexicalAndSyntaxAnalyzer analyzer = new LexicalAndSyntaxAnalyzer();
+        JovaParser parser = analyzer.createParser(analyzer.lexing(file_path, debug));
+        parser.reset();
+        ParseTree parseTree = parser.program();
+
+        if (ErrorHandler.INSTANCE.getNumParseErrors() != 0) {
+            ErrorHandler.INSTANCE.printErrorsAndWarnings();
+            return ErrorHandler.INSTANCE.getNumParseErrors();
+        }
+
+        TypeChecker typeChecker = new TypeChecker();
+        typeChecker.checkTypes(file_path, debug, false);
+
+        if (ErrorHandler.INSTANCE.getNumTypeErrors() != 0) {
+            ErrorHandler.INSTANCE.printErrorsAndWarnings();
+            return ErrorHandler.INSTANCE.getNumTypeErrors();
+        }
+
+        // generate Jasmin files
+        CodeGeneratorVisitor codeVisitor = new CodeGeneratorVisitor();
+        codeVisitor.visit(parseTree);
+
+        // optimize code
+        optimizeBinaryInstruction();
+
+        Collection<SymbolClass> symbolClasses = SymbolTable.getInstance().getClasses();
+        symbolClasses.forEach(symbolClass -> {
+            try (ClassWriter codeWriter = new ClassWriter(out_path, symbolClass)) {
+                codeWriter.write();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        SymbolTable.reset();
+
         return 0;
     }
+
+    public void optimizeBinaryInstruction() {
+        SymbolTable symbolTable = SymbolTable.getInstance();
+        symbolTable.getClasses().forEach(
+            clazz -> {
+                clazz.getMethods().forEach(
+                    method -> {
+                        LinkedList<BaseInstruction> optimizedInstructions =
+                                OptimizerUtils.constantsFolding((LinkedList<BaseInstruction>) method.getInstructions());
+                    });
+            });
+    }
+
 }
---
+++ src/main/java/at/tugraz/ist/cc/OptimizerUtils.java
@@ -0,0 +1,68 @@
+package at.tugraz.ist.cc;
+
+import at.tugraz.ist.cc.instructions.ArithmeticBinaryInstruction;
+import at.tugraz.ist.cc.instructions.BaseInstruction;
+import at.tugraz.ist.cc.instructions.OperatorTypes;
+import at.tugraz.ist.cc.symbol_table.SymbolVariable;
+
+import java.util.LinkedList;
+
+public class OptimizerUtils {
+    public OptimizerUtils() {
+    }
+
+    public static LinkedList<BaseInstruction> constantsFolding(LinkedList<BaseInstruction> instructions) {
+        LinkedList<BaseInstruction> optimizedInstructions = new LinkedList<BaseInstruction>();
+
+        instructions.forEach(
+            instruction -> {
+                if (instruction instanceof ArithmeticBinaryInstruction) {
+                    SymbolVariable lhs = ((ArithmeticBinaryInstruction) instruction).leftParam;
+                    SymbolVariable rhs = ((ArithmeticBinaryInstruction) instruction).rightParam;
+                    OperatorTypes operator = ((ArithmeticBinaryInstruction) instruction).operator;
+                    Integer result = null;
+
+                    if (lhs.getValue() != null && rhs.getValue() != null) {
+                        switch (operator) {
+                            case ADD:
+                                result = (Integer) lhs.getValue() + (Integer) rhs.getValue();
+                                break;
+
+                            case MUL:
+                                result = (Integer) lhs.getValue() * (Integer) rhs.getValue();
+                                break;
+
+                            case SUB:
+                                result = (Integer) lhs.getValue() - (Integer) rhs.getValue();
+                                break;
+
+                            case DIV:
+                                if ((Integer) rhs.getValue() != 0) {
+                                    result = (Integer) lhs.getValue() / (Integer) rhs.getValue();
+                                } else {
+                                    optimizedInstructions.add(instruction);
+                                }
+                                break;
+
+                            case MOD:
+                                if ((Integer) rhs.getValue() != 0) {
+                                    result = (Integer) lhs.getValue() % (Integer) rhs.getValue();
+                                } else {
+                                    optimizedInstructions.add(instruction);
+                                }
+                                break;
+                        }
+                        instruction.getResult().setValue(result);
+
+                    } else {
+                        optimizedInstructions.add(instruction);
+                    }
+
+                } else {
+                    optimizedInstructions.add(instruction);
+                }
+        });
+
+        return optimizedInstructions;
+    }
+}
